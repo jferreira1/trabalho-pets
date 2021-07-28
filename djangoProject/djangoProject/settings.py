f192415ea2bb0f6ddcbbdcdbf3888c8909c44a2e
Caique Evaristo <caique.evaristo@live.com>
2021-07-28 19:10:18 -0300

    Set debug to false

--- djangoProject/djangoProject/settings.py
+++ djangoProject/djangoProject/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-b7*5t=oad%#cg+jx_89fs&vmql9^$1mk)7sjyv=13kw&ap0gj#'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
